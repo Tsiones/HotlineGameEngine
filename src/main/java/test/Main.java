2d0eb61084c36be273559a59c28cc81b7541eba5
Magnus <EnderCrypt@gmail.com>
2018-01-22 14:09:26 +0100

    made a variable more properly named in Main (test)

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -4,11 +4,11 @@ import net.ddns.endertsion.gameengine.HotlineGameEngine;
 
 public class Main
 {
-	private static HotlineGameEngine hge;
+	private static HotlineGameEngine hotlineGameEngine;
 
 	public static void main(String[] args)
 	{
-		hge = new HotlineGameEngine("Lord Knows");
+		hotlineGameEngine = new HotlineGameEngine("Lord Knows");
 	}
 
 }
