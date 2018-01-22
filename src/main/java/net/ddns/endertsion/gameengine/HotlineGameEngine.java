2e74527e9e83f9567e48e81e6f6987564d38f1bc
Tsiones <new_edward@hotmail.com>
2018-01-22 13:26:02 +0100

    POM.xml file changed

--- src/main/java/net/ddns/endertsion/gameengine/HotlineGameEngine.java
+++ src/main/java/net/ddns/endertsion/gameengine/HotlineGameEngine.java
@@ -1,6 +1,8 @@
 package net.ddns.endertsion.gameengine;
 
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -12,7 +14,14 @@ public class HotlineGameEngine
 
 	public HotlineGameEngine(String title)
 	{
-		panel = new JPanel();
+		panel = new JPanel()
+		{
+			@Override
+			protected void paintComponent(Graphics g)
+			{
+				draw((Graphics2D) g);
+			}
+		};
 
 		frame = new JFrame(title);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -21,5 +30,10 @@ public class HotlineGameEngine
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
+	}
+
+	private void draw(Graphics2D g2d)
+	{
+		g2d.fillOval(2, 2, 55, 55);
 	}
 }
