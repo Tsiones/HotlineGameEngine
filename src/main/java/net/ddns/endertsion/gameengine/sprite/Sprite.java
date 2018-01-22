f73dcdbfedf657c960d9740ad421aeedc3eba522
Magnus <EnderCrypt@gmail.com>
2018-01-22 15:05:35 +0100

    added classes for sprite/images

---
+++ src/main/java/net/ddns/endertsion/gameengine/sprite/Sprite.java
@@ -0,0 +1,13 @@
+package net.ddns.endertsion.gameengine.sprite;
+
+import java.io.Serializable;
+
+public class Sprite implements Serializable
+{
+	private static final long serialVersionUID = -4503798745690910893L;
+	/**
+	 * 
+	 */
+
+	// TODO: implement
+}
---
+++ src/main/java/net/ddns/endertsion/gameengine/sprite/SpriteManager.java
@@ -0,0 +1,6 @@
+package net.ddns.endertsion.gameengine.sprite;
+
+public class SpriteManager
+{
+	// TODO: implement
+}
