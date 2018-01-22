66b3a61affe6c5418504090fb780257d39b270b1
Tsiones <new_edward@hotmail.com>
2018-01-22 15:59:05 +0100

    implemented RoomManager

--- src/main/java/net/ddns/endertsion/gameengine/room/RoomManager.java
+++ src/main/java/net/ddns/endertsion/gameengine/room/RoomManager.java
@@ -1,6 +1,28 @@
 package net.ddns.endertsion.gameengine.room;
 
+import java.util.Optional;
+
 public class RoomManager
 {
-	// TODO: implement
+	private Optional<Room> optionalRoom = Optional.empty();
+
+	public RoomManager()
+	{
+
+	}
+
+	public void setRoom(Room room)
+	{
+		optionalRoom = Optional.of(room);
+	}
+
+	public Room getRoom()
+	{
+		return optionalRoom.get();
+	}
+
+	public boolean hasRoom()
+	{
+		return optionalRoom.isPresent();
+	}
 }
